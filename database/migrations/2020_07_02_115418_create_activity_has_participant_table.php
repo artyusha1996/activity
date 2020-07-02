0de47b1dec24088855e3337c19ad343773026a5c
artur <artur.1996.harutyunyan@gmail.com>
2020-07-02 15:56:48 +0400

    Added activity_has_participant table

---
+++ database/migrations/2020_07_02_115418_create_activity_has_participant_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivityHasParticipantTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity_has_participant', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('activity_id');
+            $table->unsignedBigInteger('participant_id');
+            $table->timestamps();
+
+            $table->foreign('activity_id')
+                ->references('id')
+                ->on('activities')
+                ->onDelete('cascade');
+
+            $table->foreign('participant_id')
+                ->references('id')
+                ->on('participants')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity_has_participant');
+    }
+}
